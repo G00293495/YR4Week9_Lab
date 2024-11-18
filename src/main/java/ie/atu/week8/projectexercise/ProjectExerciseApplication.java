710d0fc910908af6ea44bc8381f6e77318d35687
G00392495@atu.ie <G00392495@atu.ie>
2024-11-18 17:06:32 +0000

    finished week9 lab

--- src/main/java/ie/atu/week8/projectexercise/ProjectExerciseApplication.java
+++ src/main/java/ie/atu/week8/projectexercise/ProjectExerciseApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjectExerciseApplication {
+public class  ProjectExerciseApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectExerciseApplication.class, args);
